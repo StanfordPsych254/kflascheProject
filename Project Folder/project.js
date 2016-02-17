14fb62a9b5d20c045abf7f6acf5e2847e7d0b8d2
kflasche <kflasche@stanford.edu>
2016-02-16 19:46:36 -0800

    Additional features
    Added timer, error message if there are unanswered questions, content
    validation for age box, and prompt response on last page if there are
    unanswered questions.

--- Project Folder/project.js
+++ Project Folder/project.js
@@ -2,22 +2,46 @@
 
 // Set condition
 $(function() {
-  var CONDITION = Math.floor(Math.random() * 2);
+  $('#age-box').filter_input({regex:'[0-9]'});
 
+  var CONDITION = Math.floor(Math.random() * 2);
+  var a;
+
+  //Instructions Slide
   $('.start-button').click(function(event) {
     $('#instructions').addClass('hidden');
     $('#condition-' + CONDITION).removeClass('hidden');
+    a = new Date();
   });
 
   // Start of condition 0 (God)
   $('#page1-god-next').click(function(event) {
     $('#condition-0').addClass('hidden');
     $('#page2-god').removeClass('hidden');
+    time = (new Date() - a)/1000;
   });
 
   $('#page2-god-next').click(function(event) {
-    $('#page2-god').addClass('hidden');
-    $('#transition').removeClass('hidden');
+    var completed = true;
+
+//Add to each slide where I want it
+    if (
+      !$("input:radio[name='god']:checked").val() ||
+      !$('#god-thoughts').val() ||
+      !$('#god-attncheck').val()
+    ) {
+      $('.error').removeClass('hidden');
+      completed = false;
+      window.scrollTo(0,0);
+    }
+    //End
+
+    if (completed) { //add this
+      $('#page2-god').addClass('hidden');
+      $('#transition').removeClass('hidden');
+      $('.error').addClass('hidden'); //add this
+    }
+
   });
   // End of condition 0 (God)
 
@@ -25,6 +49,7 @@ $(function() {
   $('#page1-control-next').click(function(event) {
     $('#condition-1').addClass('hidden');
     $('#page2-control').removeClass('hidden');
+    time = (new Date() - a)/1000;
   });
 
   $('#page2-control-next').click(function(event) {
@@ -33,37 +58,58 @@ $(function() {
   });
   // End of condition 1 (Control)
 
+  //Transition Slide
   $('#transition-next').click(function(event) {
     $('#transition').addClass('hidden');
     $('#motorcycle').removeClass('hidden');
   });
 
+  //Scenario 1: Motorcycle
   $('#motorcycle-next').click(function(event) {
     window.scrollTo(0,0);
     $('#motorcycle').addClass('hidden');
     $('#wildernesscamping').removeClass('hidden');
   });
 
+  //Scenario 2: Wilderness Camping
   $('#wildernesscamping-next').click(function(event) {
     window.scrollTo(0,0);
     $('#wildernesscamping').addClass('hidden');
     $('#skiing').removeClass('hidden');
   });
 
+  //Scenario 3: Skiing
   $('#skiing-next').click(function(event) {
     window.scrollTo(0,0);
     $('#skiing').addClass('hidden');
     $('#believe-god').removeClass('hidden');
   });
 
+  //Do you believe in God?
   $('#believe-god-next').click(function(event) {
     $('#believe-god').addClass('hidden');
     $('#final-questions').removeClass('hidden');
   });
 
+  //Demographics
   $('#submit').click(function(event) {
-    $('#final-questions').addClass('hidden');
-    $('#thankyou').removeClass('hidden');
+    var completed = true;
+    var answered = true;
+
+    if (!$("input:radio[name='gender']:checked").val() ||
+        !$("input:radio[name='english']:checked").val() ||
+        !$('#age-box').val()) {
+      answered = false;
+    }
+
+    if (!answered) {
+      completed = confirm('There are unanswered questions on this page. Are you sure you want to continue?');
+    }
+
+    if (completed) {
+      $('#final-questions').addClass('hidden');
+      $('#thankyou').removeClass('hidden');
+    }
   });
 
 })
