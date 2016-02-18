dbef3963a19678cd5b03c07d4b323a5c135c27ff
kflasche <kflasche@stanford.edu>
2016-02-17 17:54:14 -0800

    Added logging of data and extra question on last page

--- Project Folder/project.js
+++ Project Folder/project.js
@@ -150,7 +150,7 @@ $(function() {
     var completed = true;
 
     if (
-      !$("input:radio[name='god-believe']:checked").val() 
+      !$("input:radio[name='god-believe']:checked").val()
     ) {
       $('.error').removeClass('hidden');
       completed = false;
@@ -171,6 +171,7 @@ $(function() {
 
     if (!$("input:radio[name='gender']:checked").val() ||
         !$("input:radio[name='english']:checked").val() ||
+        !$("input:radio[name='check']:checked").val() ||
         !$('#age-box').val()) {
       answered = false;
     }
@@ -180,9 +181,191 @@ $(function() {
     }
 
     if (completed) {
+      experiment.log_response();
+      experiment.submit_others();
+      experiment.end();
       $('#final-questions').addClass('hidden');
       $('#thankyou').removeClass('hidden');
     }
   });
 
 })
+
+//Submission of Data
+var experiment = {
+
+    // The object to be submitted.
+    data: {
+      condition_god: [],
+      god_thoughts: [],
+      god_attncheck: [],
+      condition_control: [],
+      control_thoughts: [],
+      control_attncheck: [],
+      mlikelihood: [],
+      mserious: [],
+      mcope: [],
+      mlikely: [],
+      wclikelihood: [],
+      wcserious: [],
+      wccope: [],
+      wclikely: [],
+      slikelihood: [],
+      sserious: [],
+      s_cope: [],
+      slikely: [],
+      godbelief: [],
+      gen: [],
+      age: [],
+      engl: [],
+      previouscheck: [],
+    },
+
+    end: function() {
+    // Wait 1.5 seconds and then submit the whole experiment object to Mechanical Turk
+    //(mmturkey filters out the functions so we know we're just submitting properties [i.e. data])
+    setTimeout(function() { turk.submit(experiment.data) }, 1500);
+    },
+
+    // LOG RESPONSE
+    log_response: function() {
+      var response_logged = false;
+
+      //Array of radio buttons
+      var radio1 = document.getElementsByName("god");
+      var radio2 = document.getElementsByName("control");
+      var radio3 = document.getElementsByName("m-likelihood");
+      var radio4 = document.getElementsByName("m-serious");
+      var radio5 = document.getElementsByName("m-cope");
+      var radio6 = document.getElementsByName("m-likely");
+      var radio7 = document.getElementsByName("wc-likelihood");
+      var radio8 = document.getElementsByName("wc-serious");
+      var radio9 = document.getElementsByName("wc-cope");
+      var radio10 = document.getElementsByName("wc-likely");
+      var radio11 = document.getElementsByName("s-likelihood");
+      var radio12 = document.getElementsByName("s-serious");
+      var radio13 = document.getElementsByName("s-cope");
+      var radio14 = document.getElementsByName("s-likely");
+      var radio15 = document.getElementsByName("god-believe");
+      var radio16 = document.getElementsByName("gender");
+      var radio17 = document.getElementsByName("english");
+      var radio18 = document.getElementsByName("check");
+
+      // Loop through radio buttons
+      for (i = 0; i < radio1.length; i++) {
+        if (radio1[i].checked) {
+          experiment.data.condition_god.push(radio1[i].value);
+          response_logged = true;
+          }
+        }
+      for (i = 0; i < radio2.length; i++) {
+        if (radio2[i].checked) {
+          experiment.data.condition_control.push(radio2[i].value);
+          response_logged = true;
+          }
+        }
+      for (i = 0; i < radio3.length; i++) {
+        if (radio3[i].checked) {
+          experiment.data.mlikelihood.push(radio3[i].value);
+          response_logged = true;
+          }
+        }
+      for (i = 0; i < radio4.length; i++) {
+        if (radio4[i].checked) {
+          experiment.data.mserious.push(radio4[i].value);
+          response_logged = true;
+          }
+        }
+      for (i = 0; i < radio5.length; i++) {
+        if (radio5[i].checked) {
+          experiment.data.mcope.push(radio5[i].value);
+          response_logged = true;
+          }
+        }
+      for (i = 0; i < radio6.length; i++) {
+        if (radio6[i].checked) {
+          experiment.data.mlikely.push(radio6[i].value);
+          response_logged = true;
+          }
+        }
+      for (i = 0; i < radio7.length; i++) {
+        if (radio7[i].checked) {
+          experiment.data.wclikelihood.push(radio7[i].value);
+          response_logged = true;
+          }
+        }
+      for (i = 0; i < radio8.length; i++) {
+        if (radio8[i].checked) {
+          experiment.data.wcserious.push(radio8[i].value);
+          response_logged = true;
+          }
+        }
+      for (i = 0; i < radio9.length; i++) {
+        if (radio9[i].checked) {
+          experiment.data.wccope.push(radio9[i].value);
+          response_logged = true;
+          }
+        }
+      for (i = 0; i < radio10.length; i++) {
+        if (radio10[i].checked) {
+          experiment.data.wclikely.push(radio10[i].value);
+          response_logged = true;
+          }
+        }
+      for (i = 0; i < radio11.length; i++) {
+        if (radio11[i].checked) {
+          experiment.data.slikelihood.push(radio11[i].value);
+          response_logged = true;
+          }
+        }
+      for (i = 0; i < radio12.length; i++) {
+        if (radio12[i].checked) {
+          experiment.data.sserious.push(radio12[i].value);
+          response_logged = true;
+          }
+        }
+      for (i = 0; i < radio13.length; i++) {
+        if (radio13[i].checked) {
+          experiment.data.s_cope.push(radio13[i].value);
+          response_logged = true;
+          }
+        }
+      for (i = 0; i < radio14.length; i++) {
+        if (radio14[i].checked) {
+          experiment.data.slikely.push(radio14[i].value);
+          response_logged = true;
+          }
+        }
+      for (i = 0; i < radio15.length; i++) {
+        if (radio15[i].checked) {
+          experiment.data.godbelief.push(radio15[i].value);
+          response_logged = true;
+          }
+        }
+      for (i = 0; i < radio16.length; i++) {
+        if (radio16[i].checked) {
+          experiment.data.gen.push(radio16[i].value);
+          response_logged = true;
+          }
+        }
+      for (i = 0; i < radio17.length; i++) {
+        if (radio17[i].checked) {
+          experiment.data.engl.push(radio17[i].value);
+          response_logged = true;
+          }
+        }
+      for (i = 0; i < radio18.length; i++) {
+        if (radio18[i].checked) {
+          experiment.data.previouscheck.push(radio18[i].value);
+          response_logged = true;
+          }
+        }
+      },
+    submit_others: function() {
+      experiment.data.god_thoughts.push(document.getElementById("god-thoughts").value);
+      experiment.data.god_attncheck.push(document.getElementById("god-attncheck").value);
+      experiment.data.control_thoughts.push(document.getElementById("control-thoughts").value);
+      experiment.data.control_attncheck.push(document.getElementById("control-attncheck").value);
+      experiment.data.age.push(document.getElementById("age-box").value);
+  }
+}
